3fa437e7ecee5a6a736a8bff78fbdcda224d3740
unknown <mr09991999@outlook.com>
2024-09-19 11:01:07 +0800

    ready for build

--- src/main/java/net/tracen/tracen_delight/item/ItemRegistry.java
+++ src/main/java/net/tracen/tracen_delight/item/ItemRegistry.java
@@ -15,43 +15,43 @@ public class ItemRegistry {
 	
     public static final DeferredItem<Item> PARFAIT = register("parfait",
             () -> new UmaFoodItem(UmaStatusUtils::addMotivation,
-                    FoodInfo.builder().name("parfait").amountAndCalories(5, 0.6F).water(0F)
+                    FoodInfo.builder().name("parfait").amountAndCalories(4, 0.6F).water(0F)
                             .nutrients(2F, 2F, 0F, 0F, 0F).decayModifier(1.5F).heatCapacity(1F).cookingTemp(480F)
                             .build()));
     
     public static final DeferredItem<Item> CARROT_BURGERMEAT = register("carrot_burgermeat",
             () -> new UmaFoodItem(UmaStatusUtils::addMotivation,
-                    FoodInfo.builder().name("carrot_burgermeat").amountAndCalories(5, 0.6F).water(0F)
+                    FoodInfo.builder().name("carrot_burgermeat").amountAndCalories(6, 0.6F).water(0F)
                             .nutrients(2F, 2F, 0F, 0F, 0F).decayModifier(1.5F).heatCapacity(1F).cookingTemp(480F)
                             .build()));
     
     public static final DeferredItem<Item> CARROT_FRIED_NOODLES = register("carrot_fried_noodles",
             () -> new UmaFoodItem(UmaStatusUtils::addMotivation,
-                    FoodInfo.builder().name("carrot_fried_noodles").amountAndCalories(5, 0.6F).water(0F)
+                    FoodInfo.builder().name("carrot_fried_noodles").amountAndCalories(8, 0.8F).water(0F)
                             .nutrients(2F, 2F, 0F, 0F, 0F).decayModifier(1.5F).heatCapacity(1F).cookingTemp(480F)
                             .build()));
     
     public static final DeferredItem<Item> CARROT_SKEWERS = register("carrot_skewers",
             () -> new UmaFoodItem(UmaStatusUtils::addMotivation,
-                    FoodInfo.builder().name("carrot_skewers").amountAndCalories(5, 0.6F).water(0F)
+                    FoodInfo.builder().name("carrot_skewers").amountAndCalories(3, 0.6F).water(0F)
                             .nutrients(2F, 2F, 0F, 0F, 0F).decayModifier(1.5F).heatCapacity(1F).cookingTemp(480F)
                             .build()));
     
     public static final DeferredItem<Item> TOUGHNESS_BAR = register("toughness_bar",
             () -> new UmaFoodItem(UmaStatusUtils::addMotivation,
-                    FoodInfo.builder().name("toughness_bar").amountAndCalories(5, 0.6F).water(0F)
+                    FoodInfo.builder().name("toughness_bar").amountAndCalories(4, 0.6F).water(0F)
                             .nutrients(2F, 2F, 0F, 0F, 0F).decayModifier(1.5F).heatCapacity(1F).cookingTemp(480F)
                             .build()));
     
     public static final DeferredItem<Item> CARROT_SMOOTHIES = register("carrot_smoothies",
             () -> new UmaFoodItem(UmaStatusUtils::addMotivation,
-                    FoodInfo.builder().name("carrot_smoothies").amountAndCalories(5, 0.6F).water(0F)
+                    FoodInfo.builder().name("carrot_smoothies").amountAndCalories(4, 0.4F).water(0F)
                             .nutrients(2F, 2F, 0F, 0F, 0F).decayModifier(1.5F).heatCapacity(1F).cookingTemp(480F)
                             .build()));
     
     public static final DeferredItem<Item> CARROT_STRAWBERRY_ICE_CREAM = register("carrot_strawberry_ice_cream",
             () -> new UmaFoodItem(UmaStatusUtils::addMotivation,
-                    FoodInfo.builder().name("carrot_strawberry_ice_cream").amountAndCalories(5, 0.6F).water(0F)
+                    FoodInfo.builder().name("carrot_strawberry_ice_cream").amountAndCalories(4, 0.4F).water(0F)
                             .nutrients(2F, 2F, 0F, 0F, 0F).decayModifier(1.5F).heatCapacity(1F).cookingTemp(480F)
                             .build()));
     
